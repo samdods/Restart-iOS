e7f65deb1bdf2314c244ffed1003d126f729601e
Sam Dods <sam@theappbusiness.com>
2014-03-12 20:07:38 +0000

    Initial Commit

---
+++ Restart OS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Restart OS
+//
+//  Created by Sam Dods on 12/03/2014.
+//  Copyright (c) 2014 Sam Dods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Restart OS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Restart OS
+//
+//  Created by Sam Dods on 12/03/2014.
+//  Copyright (c) 2014 Sam Dods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
