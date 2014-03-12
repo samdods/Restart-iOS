cda987b6709bb09d73e9ed0b578e2ca9b59139d6
Sam Dods <sam@theappbusiness.com>
2014-03-12 21:00:40 +0000

    reordered statements

--- Restart OS/ViewController.m
+++ Restart OS/ViewController.m
@@ -18,11 +18,11 @@
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
   UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MyCell" forIndexPath:indexPath];
-  
-  cell.layer.cornerRadius = 1;
-  
   UIView *subView = [[UIView alloc] initWithFrame:cell.bounds];
   [cell addSubview:subView];
+  
+  // The following 3 lines of code cause the crash, all three lines are required.
+  cell.layer.cornerRadius = 1;
   subView.layer.shadowOpacity = 1;
   subView.layer.borderWidth = 1;
   
